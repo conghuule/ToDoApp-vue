e12e62044f0c1a249865d63a66e56dfee7571e53
conghuule <leconghuu.lch@gmail.com>
2024-03-20 23:29:23 +0700

    final

--- todo_app/src/components/HelloWorld.vue
+++ todo_app/src/components/HelloWorld.vue
@@ -1,58 +1,83 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="todo-app">
+    <h1>{{ title }}</h1>
+    <input type="text" v-model="newTodo" @keyup.enter="addTodo" placeholder="Add new todo"
+      class="w-full p-2 border border-gray-300 rounded mb-4">
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="(todo, index) in todos" :key="index" class="flex items-center justify-between bg-white border border-gray-300 p-4 mb-2">
+        <input type="checkbox" v-model="todo.completed" class="mr-2">
+        <span :class="{ 'line-through': todo.completed }">{{ todo.text }}</span>
+        <button @click="removeTodo(index)" class="text-red-600">Delete</button>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'TodoApp',
+  data() {
+    return {
+      title: 'Vue Todo App',
+      newTodo: '',
+      todos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push({
+          text: this.newTodo,
+          completed: false
+        });
+        this.newTodo = '';
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.todo-app {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
+
+h1 {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
 ul {
-  list-style-type: none;
   padding: 0;
+  list-style: none;
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
-a {
-  color: #42b983;
+
+input[type="checkbox"] {
+  margin-right: 10px;
+}
+
+.line-through {
+  text-decoration: line-through;
+}
+
+button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
 }
 </style>
